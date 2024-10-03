02c70c7eed5f7d49b0a6407b78d6391edc9323f8
MVKarthikReddy <karthikmedagam@gmail.com>
2024-10-03 20:35:56 +0530

    Done

--- shop/urls.py
+++ shop/urls.py
@@ -1,23 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-# from .views import ProductViewSet, CartViewSet, OrderViewSet, CategoryViewSet, lobby
+from .views import ProductViewSet, CartViewSet, OrderViewSet, CategoryViewSet, lobby
 from . import views
 
 
 # Create a router to handle viewsets
-# router = DefaultRouter()
-# router.register(r'products', ProductViewSet)
-# router.register(r'cart', CartViewSet, basename='cart')
-# router.register(r'orders', OrderViewSet, basename='orders')
-# router.register(r'categories', CategoryViewSet, basename='orders')
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+router.register(r'cart', CartViewSet, basename='cart')
+router.register(r'orders', OrderViewSet, basename='orders')
+router.register(r'categories', CategoryViewSet, basename='orders')
 
 
 
 
 
 urlpatterns = [
-    # path('', include(router.urls)),  # Include all viewsets in the router
-    path('', views.lobby),
-    # path('cart/add_to_cart/', CartViewSet.as_view({'post': 'add_to_cart'}), name='add_to_cart'),
+    path('', include(router.urls)),  # Include all viewsets in the router
+    # path('', views.lobby),
+    path('cart/add_to_cart/', CartViewSet.as_view({'post': 'add_to_cart'}), name='add_to_cart'),
 
 ]
